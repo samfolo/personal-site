65e1b58f157276b4ac44b8577635a560bef290ad
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2025-12-01 19:46:55 +0000

    feat: install MDX and TailwindCSS integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+import mdx from '@astrojs/mdx';
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
+
+  integrations: [mdx()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
