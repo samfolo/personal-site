8cee4a2ec720cf642bdb5a4747aa3e0569707c44
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2025-12-12 00:42:06 +0000

    refactor: use `.ico` over `.svg` to save hassle...

--- src/scripts/favicon.ts
+++ src/scripts/favicon.ts
@@ -7,6 +7,6 @@
 export function setFavicon(theme: string): void {
   const favicon = document.querySelector<HTMLLinkElement>('link[data-favicon]');
   if (favicon) {
-    favicon.href = `/sf-${theme}.svg`;
+    favicon.href = `/sf-${theme}.ico`;
   }
 }
