afaf9c5fac8c470f9ad59352b04da0a9583d8572
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2025-12-17 17:57:44 +0000

    refactor: componentise BlogListItem

--- src/lib/og/index.ts
+++ src/lib/og/index.ts
@@ -7,7 +7,7 @@
 export {FONTS} from "./fonts";
 export {generateOgImage} from "./generate";
 export {createOgTemplate} from "./template";
-export {getThemeFromTitle, THEME_LABELS, THEMES} from "./theme";
+export {getThemeFromTitle} from "./theme";
 
 export type {FontData} from "./fonts";
 export type {OgTemplateOptions} from "./template";
